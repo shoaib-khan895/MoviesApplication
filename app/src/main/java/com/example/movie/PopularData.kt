021cc77f3e6cb2e4a2bc1ccc1b662c22a0d25c64
shoaib.khan <shoaib.khan>
2021-10-01 16:08:59 +0530

    two fragments added, bottom nav moved to activity

---
+++ app/src/main/java/com/example/movie/PopularData.kt
@@ -0,0 +1,58 @@
+package com.example.movie
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.movie.adapter.MovieAdapter
+import com.example.movie.models.Movie
+import com.example.movie.models.MovieResponse
+import com.example.movie.services.MovieApiInterface
+import com.example.movie.services.MovieApiService
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.popular_data.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+
+class PopularData : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val newView = inflater.inflate(R.layout.popular_data, container, false)
+        return newView
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+      //  blank_text.visibility = View.GONE
+        rv_movies_list.layoutManager = LinearLayoutManager(activity)
+        rv_movies_list.setHasFixedSize(true)
+        tv_sub_title.setText(R.string.popular)
+        getMovieData { movies: List<Movie> ->
+            rv_movies_list.adapter = MovieAdapter(movies)
+        }
+    }
+
+    fun getMovieData(Callback: (List<Movie>) -> Unit) {
+        val apiService = MovieApiService.getInstance().create(MovieApiInterface::class.java)
+        apiService.getMovieList().enqueue(object : Callback<MovieResponse> {
+            override fun onResponse(
+                call: Call<MovieResponse>, response: Response<MovieResponse>
+            ) {
+                return Callback(response.body()!!.movies)
+            }
+
+            override fun onFailure(call: Call<MovieResponse>, t: Throwable) {
+                TODO("Not yet implemented")
+            }
+        })
+    }
+}
---
+++ app/src/main/java/com/example/movie/UpcomingData.kt
@@ -0,0 +1,63 @@
+package com.example.movie
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.movie.adapter.UpcomingAdapter
+import com.example.movie.models.MovieUpcoming
+import com.example.movie.models.MovieUpcomingResponse
+import com.example.movie.services.MovieApiInterface
+import com.example.movie.services.MovieApiService
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.upcoming_data.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+
+class UpcomingData : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.upcoming_data, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+       // blank_text.visibility = View.GONE
+        // Respond to navigation item 2 click
+        rv_movies_list.layoutManager = LinearLayoutManager(activity)
+        rv_movies_list.setHasFixedSize(true)
+        tv_sub_title.setText(R.string.Up_Coming)
+        getUpcomingMovieData { upcoming: List<MovieUpcoming> ->
+            rv_movies_list.adapter = UpcomingAdapter(upcoming)
+        }
+
+
+    }
+
+    private fun getUpcomingMovieData(Callback: (List<MovieUpcoming>) -> Unit) {
+        val apiServices =
+            MovieApiService.getInstance().create(MovieApiInterface::class.java)
+        apiServices.getUpcomingList().enqueue(object : Callback<MovieUpcomingResponse> {
+            override fun onResponse(
+                call: Call<MovieUpcomingResponse>,
+                response: Response<MovieUpcomingResponse>
+            ) {
+                return Callback(response.body()!!.upcoming)
+            }
+
+            override fun onFailure(call: Call<MovieUpcomingResponse>, t: Throwable) {
+                TODO("Not yet implemented")
+            }
+        })
+
+    }
+}
